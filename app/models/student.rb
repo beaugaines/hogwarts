0f713e278b24e653616bf9bcad687945c0166349
Charlie Gaines <beaugaines@gmail.com>
2014-02-21 15:44:35 -0500

    superfluous navigation links

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ActiveRecord::Base
   attr_accessible :name, :house_id
   belongs_to :house
+  delegate :name, to: :house, prefix: true
 
   after_create :assign_house
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Hogwarts::Application.routes.draw do
   
   resources :houses, only: [:index, :show]
-  resources :students, only: [:index, :new, :create]
+  resources :students, only: [:index, :new, :create, :show]
 
   root to: 'welcome#index'
 end
